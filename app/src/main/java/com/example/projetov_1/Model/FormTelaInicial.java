64f3b83d24dd09fcea136c6af69a34637ca98764
KyleMSJ <caiomarcelosj3@gmail.com>
2022-12-08 21:55:20 -0300

    - Criação dos fragments e navigation - inicio da tela dos chats

--- app/src/main/java/com/example/projetov_1/Model/FormTelaInicial.java
+++ app/src/main/java/com/example/projetov_1/Model/FormTelaInicial.java
@@ -3,14 +3,33 @@ package com.example.projetov_1.Model;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.projetov_1.R;
+import com.example.projetov_1.databinding.ActivityFormTelaInicialBinding;
+import com.example.projetov_1.databinding.ActivityMainBinding;
 
 public class FormTelaInicial extends AppCompatActivity {
+    private ActivityFormTelaInicialBinding binding;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_tela_inicial);
+        binding = ActivityFormTelaInicialBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initNavigation();
+    }
+
+    private void initNavigation(){
+        navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().
+                        findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation,navController);
     }
 }
